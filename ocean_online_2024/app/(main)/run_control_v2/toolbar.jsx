39df182d8c74694b50ce124fc21c37538e8678d0
DESKTOP-R3PHC6Q\SARA <yotin980@gmail.com>
2024-03-06 14:15:01 +0700

    Revert "Revert "fix demo 3 , bug when unassign""
    This reverts commit d7390df59412488c22ef5e41658f9e6edf2eb273.

--- ocean_online_2024/app/(main)/run_control_v2/toolbar.jsx
+++ ocean_online_2024/app/(main)/run_control_v2/toolbar.jsx
@@ -6,7 +6,7 @@ import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
 
-function Toolbar() {
+function Toolbar(props) {
 
     const [date, setDate] = useState(new Date);
     const [checked, setChecked] = useState(false);
@@ -23,11 +23,17 @@ function Toolbar() {
         { name: 'Service Stop', code: 'SS' },
         { name: 'Vehicle Stop', code: 'VS' },
     ];
+
+    function refreshPage(){
+        props.onRefreshPage();
+    }
+
+
     return (
         <>
             <div className="flex flex-wrap  gap-3 p-fluid p-2 ">
                 <div className="flex justify-items-center align-items-center h-2rem">
-                    <Button className='h-2rem text-sm p-2' label="Refresh" icon="pi pi-refresh" />
+                    <Button className='h-2rem text-sm p-2' label="Refresh" icon="pi pi-refresh" onClick={refreshPage}/>
                 </div>
                 <div className="flex justify-items-center align-items-center h-2rem">
                     <label>Work Date</label>
